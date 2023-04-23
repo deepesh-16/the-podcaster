6e4e964e25c71a5c4083ed2a2211a4b8415e97ef
Gautham Mallipeddi <83275001+gautham-8@users.noreply.github.com>
2023-04-23 19:24:14 +0530

    minor

--- src/Components/pages/PodcastCard.js
+++ src/Components/pages/PodcastCard.js
@@ -47,12 +47,10 @@ function PodcastCard(props) {
                 </div>
                 <div class="flex flex-col items-center pb-10">
                     <img class="w-24 h-24 mb-3 rounded shadow-lg" src={props.thumbnail} alt={props.name}/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{props.name}</h5>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white text-center">{props.name}</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">{props.description}</span>
                     <div class="flex mt-4 space-x-3 md:mt-6">
                         <button onClick={openModal} className='bg-gray-800 text-white p-2 rounded'>Listen to this podcast!</button>
-                        {/* <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
-                        {/* <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a> */}
                     </div>
                 </div>
             </div>
@@ -62,7 +60,7 @@ function PodcastCard(props) {
                 style={customStyles}
                 ariaHideApp={false}
             >
-                <img className="hidden sm:block w-52 rounded-full m-auto pb-2" src={props.thumbnail} alt={props.name} />
+                <img className="hidden sm:block w-52 rounded m-auto pb-2" src={props.thumbnail} alt={props.name} />
                 <div className="text-center">
                     <div className="font-bold text-2xl inline mr-2">
                         {props.name}
@@ -90,6 +88,7 @@ function PodcastCard(props) {
                         props.type==='audio' &&
                         <div>
                             <audio
+                                className='m-auto'
                                 ref={audioRef}
                                 controls
                                 onTimeUpdate={handleTimeUpdateAudio}
@@ -97,7 +96,7 @@ function PodcastCard(props) {
                             >
                                 Your browser does not support the audio tag.
                             </audio>
-                            <p>Current time: {currentTime.toFixed(2)}</p>
+                            <p className='text-center'>Current time: {currentTime.toFixed(2)}</p>
                         </div>
                     }
             </Modal>
